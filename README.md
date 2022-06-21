e45586bbcc9dd829b86b5ac93502beddf010180e
Pedro Henrique <pedrohesm@gmail.com>
2022-06-21 00:43:38 -0300

    create buckets and editing README.md e install Collecticfast.

--- README.md
+++ README.md
@@ -3,5 +3,17 @@
 Projeto do curso Python Pro do Renzo Nuccitelli
 aplicação disponivel em https://pythonprocourse.herokuapp.com/
 <br>
+*** Para instalar o projeto você deve fazer o git clone 
+<br>
+** após baixar o projeto, abrir o projeto na sua IDE de preferência
+<br>
+** Abrir o terminal:
+<br>
+* python3 -m venv .venv
+* source .venv/bin/activate
+* pip install -r requirements.txt
+* python manage.py runserver
+<br>
+
 [![Build Status](https://app.travis-ci.com/pedrokasak/pythonpro.svg?token=xktmnEyZWoKd5RxeaEAZ&branch=master)](https://app.travis-ci.com/pedrokasak/pythonpro)
 [![codecov](https://codecov.io/gh/pedrokasak/pythonpro/branch/master/graph/badge.svg?token=UDFAT8P3DR)](https://codecov.io/gh/pedrokasak/pythonpro)
--- pypro/settings.py
+++ pypro/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'collectfast',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
 ]
@@ -105,6 +106,45 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+COLLECTFAST_ENABLED = False
+
+# STORAGE CONFIGURATIONS IN S3 AWS
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+if AWS_ACCESS_KEY_ID:
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
+    AWS_PRELOAD_METADATA = True
+    AWS_AUTO_CREATE_BUCKET = False
+    AWS_QUERYSTRING_AUTH = True
+    AWS_S3_CUSTOM_DOMAIN = None
+
+    COLLECTFAST_ENABLED = True
+
+    AWS_DEFAULT_ACL = 'private'
+    # Static Assents
+    # ------------------------------------------------------------------
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    STATIC_S3_PATH = 'static'
+    STATIC_ROOT = f'/{STATIC_S3_PATH}/'
+    STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+    # Upload media Folder
+    # ------------------------------------------------------------------
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    DEFAULT_S3_PATH = 'media'
+    MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
+    MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'
+
+    INSTALLED_APPS.append('s3_folder_storage')
+    INSTALLED_APPS.append('storages')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
