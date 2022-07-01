22ccbd5e7b201d7a16aae96c34bee3c18e3b88f4
Pedro Henrique <pedrohesm@gmail.com>
2022-06-30 22:53:29 -0300

    Criando models User e o superuser

--- pypro/settings.py
+++ pypro/settings.py
@@ -19,6 +19,8 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())  # ['*']
 
+AUTH_USER_MODEL = 'core.User'
+
 # Application definition
 
 INSTALLED_APPS = [
